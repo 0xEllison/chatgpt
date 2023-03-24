84cd1062d0b492497955d5089da87c53c107a648
0xEllison <ellison_hsu@outlook.com>
2023-03-24 13:05:42 +0800

    完善界面，修改提交时的相对路径

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var compression = require('compression')
 var router = express.Router();
 
-const apiKey = "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";
+const apiKey = "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";
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -15,26 +15,33 @@ router.get("/chat", function (req, res, next) {
 });
 
 /* GET home page. */
-router.post("/sendMsg", async function (req, res, next) {
+router.post("/", async function (req, res, next) {
   const { ChatGPTAPI } = await import("chatgpt");
 
-  const chatGPTApi = new ChatGPTAPI({
-    apiKey,
-  });
-  // const result = await chatGPTApi.sendMessage(req.body.msg);
-  // res.send(result.text);
-  let dataList = [];
-  res.setHeader("Content-type", "application/octet-stream");
-  let dataIndex = 0;
-  const result = await chatGPTApi.sendMessage(req.body.msg, {
-    // print the partial response as the AI is "typing"
-    onProgress: (partialResponse) => {
-      let currenData = partialResponse.text.substring(dataIndex,partialResponse.text.length-1);
-      dataIndex = partialResponse.text.length-1;
-      res.write(currenData);
-      res.flush();
-    },
-  });
+  try {
+    
+    const chatGPTApi = new ChatGPTAPI({
+      apiKey,
+    });
+    // const result = await chatGPTApi.sendMessage(req.body.msg);
+    // res.send(result.text);
+    let dataList = [];
+    res.setHeader("Content-type", "application/octet-stream");
+    let dataIndex = 0;
+    const result = await chatGPTApi.sendMessage(req.body.msg, {
+      // print the partial response as the AI is "typing"
+      onProgress: (partialResponse) => {
+        let currenData = partialResponse.text.substring(dataIndex,partialResponse.text.length-1);
+        dataIndex = partialResponse.text.length-1;
+        res.write(currenData);
+        res.flush();
+      },
+    });
+  } catch (error) {
+    
+    res.write("erro try again");
+    res.flush();
+  }
   res.end();
 });
 module.exports = router;
